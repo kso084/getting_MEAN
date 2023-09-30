fa3a2abfb3f4c9530bf89db63ead9652d83fab96
kso084 <121861496+kso084@users.noreply.github.com>
2023-09-30 14:30:03 +0200

    first commit. Work up to 4.4.3. location-info.jade is a little off still

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,13 @@
+//GET "home" page
+module.exports.homelist = function(req,res){
+    res.render('location-list',{title:'Home'})
+}
+//GET "location info" page
+module.exports.locationInfo = function(req,res){
+    res.render('location-info',{title:'LocationInfo'})
+}
+//GET "add review" page
+module.exports.addReview = function(req,res){
+    res.render('location-review-form',{title:'Add review'})
+}
+
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -8,13 +8,22 @@ router.get('/', function(req, res, next) {
 */
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/Others');
+
+//Location pages
+router.get('/',ctrlLocations.homelist);
+router.get('/location',ctrlLocations.locationInfo);
+router.get('/location/review/new',ctrlLocations.addReview);
+
+//Other pages
+router.get('/about',ctrlOthers.about);
 
 /*var hompageController = function (req,res){
   res.render('index',{title:'Express'});
 };*/
 
 // GET home page. 
-router.get('/',ctrlMain.index);
+//router.get('/',ctrlMain.index);
 
 module.exports = router;
